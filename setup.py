b604cc55473c95516f668e4167afde1fffe77d74
liragabriel <liragabrieler@gmail.com>
2020-03-19 09:18:17 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='work-at-olist',
+      version='1.0.0',
+      description='Test to work at Olist',
+      url='https://github.com/liragabriel/work-at-olist',
+      author='Gabriel Lira',
+      author_email='contact@liragabriel.com',
+      packages=['books_app'],
+      install_requires=['flask'],
+      zip_safe=False)
